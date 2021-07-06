aa2ad5573ca803bbaa88bb690eef72f6d656664d
Rob C <hyakuhei@gmail.com>
2021-07-06 09:11:14 -0700

    Nested Wrappables now present in Dataflow descriptions

--- fluentm.py
+++ fluentm.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from enum import Flag, auto
+from types import WrapperDescriptorType
 from typing import Union
 import logging
 
@@ -9,9 +10,11 @@ from jinja2 import FileSystemLoader, Environment
 
 SPACES = "   "
 
+
 class Unset(object):
-    pass        
-        
+    pass
+
+
 class WrappableProtocol(object):
     def __init__(
         self,
@@ -21,7 +24,7 @@ class WrappableProtocol(object):
         clientAuthenticated: Union[Unset, bool],
         serverCredential: Union[Unset, str, None],
         clientCredential: Union[Unset, str, None],
-        version: Union[Unset, str]
+        version: Union[Unset, str],
     ):
         if isinstance(toWrap, (WrappableProtocol, Data)):
             self.wraps = toWrap
@@ -46,120 +49,160 @@ class WrappableProtocol(object):
         # ...
         print(f"{SPACES*depth} Wraps:")
         if isinstance(self.wraps, WrappableProtocol):
-            self.wraps.printChain(depth=depth+1)
+            self.wraps.printChain(depth=depth + 1)
         else:
             print(f"{SPACES* (depth+1)} {self.wraps}")
 
     # Recurse through all nested/wraped protocols and return the data that's ultimately wrapped
     def getData(self):
         if isinstance(self.wraps, Data):
-            return self.wraps
+            return self.wraps  # Base case
         else:
             return self.wraps.getData()
+
+    # Recurse and generate a single line str for the wrappable
+    def flatString(self, depth=0, s=None):
+        if s is None:
+            s = ""
+
+        if isinstance(self, WrappableProtocol):
+            s += f"{self.__class__.__name__}("
+            if isinstance(self.wraps, WrappableProtocol):
+                return self.wraps.flatString(depth=depth + 1, s=s)
+            if isinstance(self.wraps, Data):
+                s += f"{self.wraps.name}{')'*(depth+1)}"
+                return s
+        else:
+            assert False, "Bad instance type in WrappableProtocol structure"
+
+    def __str__(self):
+        return self.flatString()
+
 
 class Plaintext(WrappableProtocol):
     def __init__(self, toWrap):
         super().__init__(
             toWrap,
-            encrypted = False,
-            serverAuthenticated = False,
-            clientAuthenticated = False,
-            serverCredential=None, #TODO: Replace with a type? Would that be useful?
+            encrypted=False,
+            serverAuthenticated=False,
+            clientAuthenticated=False,
+            serverCredential=None,  # TODO: Replace with a type? Would that be useful?
             clientCredential=None,
-            version=None
+            version=None,
         )
+
 
 class IPSEC(WrappableProtocol):
     def __init__(self, toWrap):
         super().__init__(
             toWrap,
-            encrypted = True,
-            serverAuthenticated = True,
-            clientAuthenticated = True,
-            serverCredential="x509", #TODO: Replace with a type? Would that be useful?
+            encrypted=True,
+            serverAuthenticated=True,
+            clientAuthenticated=True,
+            serverCredential="x509",  # TODO: Replace with a type? Would that be useful?
             clientCredential="x509",
         )
+
 
 class TLSVPN(WrappableProtocol):
     def __init__(self, toWrap):
         super().__init__(
             toWrap,
-            encrytped = True,
-            serverAuthenticated = True,
-            clientAuthenticated = False,
-            serverCredential="x509", #TODO: Replace with a type? Would that be useful?
-            clientCredential=None
+            encrytped=True,
+            serverAuthenticated=True,
+            clientAuthenticated=False,
+            serverCredential="x509",  # TODO: Replace with a type? Would that be useful?
+            clientCredential=None,
         )
+
 
 class MTLSVPN(WrappableProtocol):
     def __init__(self, toWrap):
         super().__init__(
             toWrap,
-            encrytped = True,
-            serverAuthenticated = True,
-            clientAuthenticated = True,
+            encrytped=True,
+            serverAuthenticated=True,
+            clientAuthenticated=True,
             serverCredential="x509",
-            clientCredential="x509"
+            clientCredential="x509",
         )
+
+
+class SQL(WrappableProtocol):
+    def __init__(self, toWrap, version="0"):
+        super().__init__(
+            toWrap,
+            encrypted=False,
+            serverAuthenticated=False,
+            clientAuthenticated=True,
+            serverCredential="Username/Password",
+            clientCredential=None,
+            version=version,
+        )
+
 
 class TLS(WrappableProtocol):
     def __init__(self, toWrap, version="1.2"):
         super().__init__(
             toWrap,
-            encrypted = True,
-            serverAuthenticated= True,
+            encrypted=True,
+            serverAuthenticated=True,
             clientAuthenticated=False,
             serverCredential="x509",
             clientCredential=None,
-            version=version
+            version=version,
         )
+
 
 class TLS(WrappableProtocol):
     def __init__(self, toWrap, version="1.2"):
         super().__init__(
             toWrap,
-            encrypted = True,
-            serverAuthenticated= True,
+            encrypted=True,
+            serverAuthenticated=True,
             clientAuthenticated=False,
             serverCredential="x509",
             clientCredential=None,
-            version=version
+            version=version,
         )
-    
+
+
 class SIGV4(WrappableProtocol):
     def __init__(self, toWrap):
         super().__init__(
             toWrap,
-            encrypted = False,
+            encrypted=False,
             serverAuthenticated=False,
             clientAuthenticated=False,
             serverCredential=None,
             clientCredential="rsa",
-            version=None
-        )    
+            version=None,
+        )
+
 
 class HTTPBasicAuth(WrappableProtocol):
     def __init__(self, toWrap, version="2.0"):
         super().__init__(
             toWrap,
-            encrypted = False,
+            encrypted=False,
             serverAuthenticated=False,
             clientAuthenticated=True,
             serverCredential="HTTP Basic Auth",
             clientCredential="Username / Password",
-            version=version
+            version=version,
         )
+
 
 class HTTP(WrappableProtocol):
     def __init__(self, toWrap, version="2.0"):
         super().__init__(
             toWrap,
-            encrypted = False,
+            encrypted=False,
             serverAuthenticated=False,
             clientAuthenticated=False,
             serverCredential=None,
             clientCredential=None,
-            version=version
+            version=version,
         )
 
 
@@ -205,22 +248,22 @@ class Asset(object):
         elif isinstance(data, Data):
             theData = data
         else:
-            assert("processesData called without a data object or a string key to a data object")            
-        
+            assert "processesData called without a data object or a string key to a data object"
+
         if hasattr(self, "processedData"):
             self.processedData.append(theData)
         else:
             self.processedData = [theData]
         return self
 
-    #static / non-instantiated i.e no 'self'
+    # static / non-instantiated i.e no 'self'
     def get(className, instanceName):
         assert className in Asset._instances
 
         if instanceName in Asset._instances[className]:
             return Asset._instances[className][instanceName]
         else:
-            #TODO: Think about what exception to throw here
+            # TODO: Think about what exception to throw here
             assert False, f"Unable to find {className} of type {instanceName}"
             return None
 
@@ -231,10 +274,11 @@ class Asset(object):
 class Boundary(Asset):
     def __init__(self, name):
         super().__init__(name)
-        self.shape = "Dotted Box"        
+        self.shape = "Dotted Box"
 
     def get(name):
         return Asset.get("Boundary", name)
+
 
 class Lifetime(Flag):
     EPHEMERAL = auto()  # Less than an hour
@@ -242,15 +286,17 @@ class Lifetime(Flag):
     ANNUAL = auto()  # A year
     BIANNUAL = auto()  # Every two years
 
+
 class Classification(Flag):
     # Exposure results in complete compromise of at least one customer _or_ significant impact to more than one.
-    TOPSECRET = (auto())  
+    TOPSECRET = auto()
     # SECRET Exposure restuls in signficant impact to at least one customer
-    SECRET = auto()  
+    SECRET = auto()
     # SENSITIVE Embarassing to loose control of this but otherwise unimportant
-    SENSITIVE = auto()  
+    SENSITIVE = auto()
     # PUBLIC We'd be happy to publish this in our blogs
-    PUBLIC = auto()  
+    PUBLIC = auto()
+
 
 class Credential(Asset):
     def __init__(self, name):
@@ -275,7 +321,7 @@ class Credential(Asset):
 
     def hasLifetime(self, lifetime):
         assert isinstance(lifetime, Lifetime)
-        self.lifetime = lifetime        
+        self.lifetime = lifetime
         return self
 
     def isRevokable(self):
@@ -289,10 +335,12 @@ class Credential(Asset):
     def get(name):
         return Asset.get("Credential", name)
 
+
 class Container(Asset):
     def __init__(self, name):
         super().__init__(name)
         self.shape = "Circle"
+
 
 class Data(Asset):
     def __init__(self, name):
@@ -313,97 +361,115 @@ class Data(Asset):
     def get(name):
         return Asset.get("Data", name)
 
+
 class Actor(Asset):
     def __init__(self, name):
         super().__init__(name)
         self.shape = "Man"
-    
+
     def get(name):
         return Asset.get("Actor", name)
+
 
 class Process(Asset):
     def __init__(self, name):
         super().__init__(name)
         self.shape = "Square"
-    
+
     def get(name):
         return Asset.get("Process", name)
+
 
 # DataFlow is _NOT_ an Asset
 class DataFlow(object):
     _instances = {}
 
     #
-    def __init__(self, pitcher: Union[Actor, Process], catcher: Union[Actor, Process], data: Union[str, WrappableProtocol, Data], label:Union[str,None] = None, credential: Union[Unset, Credential, None]= Unset()):
-        assert isinstance(pitcher, (Actor, Process)), f"pitcher is incorrect type: {pitcher.__class__.__name__}" # Check pitcher is a type that can initiate a dataflow
-        assert isinstance(catcher, (Actor, Process)), f"catcher is incorrect type: {catcher.__class__.__name__}" # Check catcher is a type that can receive a dataflow
+    def __init__(
+        self,
+        pitcher: Union[Actor, Process],
+        catcher: Union[Actor, Process],
+        data: Union[str, WrappableProtocol, Data],
+        label: Union[str, None] = None,
+        credential: Union[Unset, Credential, None] = Unset()
+    ):
+        assert isinstance(
+            pitcher, (Actor, Process)
+        ), f"pitcher is incorrect type: {pitcher.__class__.__name__}"  # Check pitcher is a type that can initiate a dataflow
+        assert isinstance(
+            catcher, (Actor, Process)
+        ), f"catcher is incorrect type: {catcher.__class__.__name__}"  # Check catcher is a type that can receive a dataflow
 
         if isinstance(data, str):
-            logging.warning(f"DataFlow using 'string' for data. Assuming plaintext. See https://github.com/hyakuhei/fluentm/blob/main/help.md")
-            theData = Plaintext(Data(data))
+            logging.warning(
+                f"DataFlow using 'string' for data. Assuming plaintext wrapping. See https://github.com/hyakuhei/fluentm/blob/main/help.md"
+            )
+            wrappedData = Plaintext(Data(data))
         elif isinstance(data, Data):
-            logging.warning(f"DataFlow using 'Data', assuming plaintext")
-            theData = Plaintext(data)
+            logging.warning(
+                f"DataFlow using 'Data'. Assuming plaintext wrapping. See https://github.com/hyakuhei/fluentm/blob/main/help.md"
+            )
+            wrappedData = Plaintext(data)
         elif isinstance(data, WrappableProtocol):
-            theData = data
+            wrappedData = data
         else:
-            logging.error("DataFlow called with unrecognized data type, String, WrappableProtocol or Data are all acceptable ")
-        
+            logging.error(
+                "DataFlow called with unrecognized data type, String, WrappableProtocol or Data are all acceptable "
+            )
+
         name = ""
         if label is not None:
             name = label
         else:
-            name = theData.getData().name
+            name = wrappedData.getData().name
 
         if name not in DataFlow._instances:
             self.pitcher = pitcher
             self.catcher = catcher
             self.name = name
-            if data != None:
-                if isinstance(data, Data):
-                    self.data = [data]
-                if isinstance(data, list): # validated above
-                    self.data = data
+            self.wrappedData = wrappedData
             DataFlow._instances[name] = self
         else:
-            self = DataFlow._instances[name] 
+            self = DataFlow._instances[name]
 
     def __repr__(self):
         return f"{self.__class__.__name__}:{self.name}"
 
-def renderDfd(graph:Digraph, title:str, outputDir:str):
-    graph.render(f'{outputDir}/{title}-dfd', format='png', view=False)
+
+def renderDfd(graph: Digraph, title: str, outputDir: str):
+    graph.render(f"{outputDir}/{title}-dfd", format="png", view=False)
     print(graph)
     return f"{title}-dfd.png"
 
-def dfd(scenes:dict, title:str, dfdLabels=True, render=False):
+
+def dfd(scenes: dict, title: str, dfdLabels=True, render=False):
     graph = Digraph(title)
-    graph.attr(rankdir='LR', color="blue")
-    graph.attr('node', fontname='Arial', fontsize='14')
-    
+    graph.attr(rankdir="LR", color="blue")
+    graph.attr("node", fontname="Arial", fontsize="14")
+
     # Go through the dataflows, create graphs (where there's boundaries) and nodes
     for flow in scenes[title]:
-        for e in (flow.pitcher, flow.catcher):  
+        for e in (flow.pitcher, flow.catcher):
             if hasattr(e, "boundary"):
                 if not hasattr(e.boundary, "_cluster"):
                     e.boundary._cluster = Digraph(f"cluster_{e.boundary.name}")
-                    e.boundary._cluster.attr(color='red',label=e.boundary.name)
-                
+                    e.boundary._cluster.attr(color="red", label=e.boundary.name)
+
                 if not hasattr(e, "_node"):
                     e._node = e.boundary._cluster.node(e.name)
             else:
                 if not hasattr(e, "_node"):
                     print(f"Added graph node {e.name}")
                     e._node = graph.node(e.name)
-    
-    # Gather up the boundaries and look for a subgraph 
+
+    # Gather up the boundaries and look for a subgraph
     baseBoundaries = []
     for flow in scenes[title]:
         for e in (flow.pitcher, flow.catcher):
             if hasattr(e, "boundary"):
                 if e.boundary not in baseBoundaries:
                     baseBoundaries.append(e.boundary)
-    
+
     for b in baseBoundaries:
         # See if this boundary has a parent boundary
         if hasattr(b, "boundary"):
@@ -416,16 +482,20 @@ def dfd(scenes:dict, title:str, dfdLabels=True, render=False):
                 parent._cluster = Digraph(f"cluster_{parent.name}")
                 parent._cluster.attr(label=parent.name, color="red")
                 parent._cluster.subgraph(b._cluster)
-            
+
             if parent not in baseBoundaries:
                 baseBoundaries.append(parent)
         else:
             graph.subgraph(b._cluster)
-    
+
     flowcounter = 1
     for flow in scenes[title]:
         if dfdLabels:
-            graph.edge(flow.pitcher.name, flow.catcher.name, label=f"({flowcounter}) {flow.name}")
+            graph.edge(
+                flow.pitcher.name,
+                flow.catcher.name,
+                label=f"({flowcounter}) {flow.name}",
+            )
         else:
             graph.edge(flow.pitcher.name, flow.catcher.name, label=f"({flowcounter})")
         flowcounter += 1
@@ -437,12 +507,14 @@ def dataFlowTable(scenes: dict, key: str):
     table = []
     flowCounter = 1
     for f in scenes[key]:
-        table.append({
-            'Flow ID':flowCounter,
-            'Pitcher':f.pitcher.name,
-            'Catcher':f.catcher.name,
-            'Data Flow':f.name
-        })
+        table.append(
+            {
+                "Flow ID": flowCounter,
+                "Pitcher": f.pitcher.name,
+                "Catcher": f.catcher.name,
+                "Data Flow": f.wrappedData.flatString(),
+            }
+        )
         flowCounter += 1
     return table
 
@@ -454,16 +526,17 @@ def report(scenes: dict, outputDir: str, select=None, dfdLabels=True):
 
     for key in select:
         graph = dfd(scenes, key, dfdLabels=dfdLabels)
-        sceneReports[key]={
-            'graph': graph,
-            'dfdImage': renderDfd(graph, key, outputDir=outputDir),
-            'dataFlowTable': dataFlowTable(scenes, key)
+        sceneReports[key] = {
+            "graph": graph,
+            "dfdImage": renderDfd(graph, key, outputDir=outputDir),
+            "dataFlowTable": dataFlowTable(scenes, key),
         }
 
     templateLoader = FileSystemLoader(searchpath="./")
     templateEnv = Environment(loader=templateLoader)
     template = templateEnv.get_template("reportTemplate.html")
 
-    with open(f"{outputDir}/ThreatModel.html","w") as f:
-        f.write(template.render({'title':"Threat Models", 'sceneReports':sceneReports}))
-
+    with open(f"{outputDir}/ThreatModel.html", "w") as f:
+        f.write(
+            template.render({"title": "Threat Models", "sceneReports": sceneReports})
+        )
